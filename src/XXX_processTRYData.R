c6c87d174e0c742645ce5ee30eee0afd811c69b1
Henry Dawson <hdd29@cornell.edu>
2025-08-12 14:45:51 -0400

    dataprocessing done!

--- src/XXX_processTRYData.R
+++ src/XXX_processTRYData.R
@@ -22,8 +22,7 @@ setDT(fixation_status)
 
 #how many of fixation_status$AccSpeciesName are in tax_data$Organism
 
-intx <- intersect(fixation_status$AccSpeciesName, tax_data$Organism)
-length(unique(intx))
+
 
 fixation_status[, FixationLabel := fcase(
   tolower(OrigValueStr) %in% c("n-fixer", "n fixer", "n2 fixing", "yes", "y", "true", "1", "yes, an n fixer", "high"), "Yes",
@@ -31,8 +30,21 @@ fixation_status[, FixationLabel := fcase(
   default = "Uncertain"
 )]
 
-table(fixation_status$FixationLabel)
-write.csv(fixation_status, "data/fixationStatus.csv")
+fixation_status[, .(n_labels = uniqueN(FixationLabel)), by = AccSpeciesName][n_labels > 1]
+
+conflicts <- fixation_status[AccSpeciesName %in% fixation_status[, .N, by = .(AccSpeciesName, FixationLabel)][, unique(AccSpeciesName[duplicated(AccSpeciesName)])]]
+
+cleaned <- fixation_status[FixationLabel != "Uncertain"]
+resolved <- cleaned[, .N, by = .(AccSpeciesName, FixationLabel)][
+  order(-N), .SD[1], by = AccSpeciesName
+][, .(AccSpeciesName, FixationLabel)]
+
+table(resolved$FixationLabel)
+
+intx <- intersect(resolved$AccSpeciesName, tax_data$Organism)
+length(unique(intx))
+
+write.csv(resolved, "data/fixationStatus.csv")
 
 phData <- rtry::rtry_import("data/TRY_data/42869_15072025181342/42869.txt")
 
@@ -61,7 +73,11 @@ setDT(clean_phData)
 
 #how many of fixation_status$AccSpeciesName are in tax_data$Organism
 
-intx <- intersect(clean_phData$AccSpeciesName, tax_data$Organism)
+resolved_phData <- clean_phData[, .N, by = .(AccSpeciesName, OrigValueStr)][
+  order(-N), .SD[1], by = AccSpeciesName
+][, .(AccSpeciesName, OrigValueStr)]
+
+intx <- intersect(resolved_phData$AccSpeciesName, tax_data$Organism)
 length(unique(intx))
 
-write.csv(clean_phData,"data/photosyntheticPathways.csv")
+write.csv(resolved_phData,"data/photosyntheticPathways.csv")
